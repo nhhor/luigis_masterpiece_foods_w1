4b6b4aecd8de69c57a40b8a9d19df299c7fae88c
nhhor <nhhor@post.com>
2020-01-17 09:13:44 -0800

    initialize project with rails template.

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Product do
+  # it { should have_many(:employees) }
+  it { should have_and_belong_to_many(:employees) }
+end
+
+# describe Division do
+#   it("titleizes the name of an employee") do
+#     division = Division.create({division_name: "giant steps"})
+#     expect(division.division_name()).to(eq("Giant Steps"))
+#   end
+# end
