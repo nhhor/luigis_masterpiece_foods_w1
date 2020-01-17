28a8b76756a257dc7123f5fbc12d37bbbca997a6
nhhor <nhhor@post.com>
2020-01-17 13:08:06 -0800

    add 2 validations to app-models-review.rb . 10 of 10 tests passing.

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,7 @@ class Review < ApplicationRecord
   validates :content_body, presence: true
   validates :rating, presence: true
 
+  validates_numericality_of :rating, greater_than_or_equal_to: 1
+  validates_numericality_of :rating, less_than_or_equal_to: 5
+
 end
