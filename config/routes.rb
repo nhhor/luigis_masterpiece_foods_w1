b513da5d841c3630deff6132983ac3e01004954b
nhhor <nhhor@post.com>
2020-01-17 10:38:41 -0800

    add app.views.products for crud.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'products#index'
+  # root to: 'products#index'
+  root to: 'products#welcome'
+
   resources :products do
     resources :reviews
   end
