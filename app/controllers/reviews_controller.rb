6393c69b8ef5b3da5512bf0533b10e586d1cb50a
nhhor <nhhor@post.com>
2020-01-17 11:26:31 -0800

    add reviews.new and .show erbs.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,44 +6,47 @@ class ReviewsController < ApplicationController
   # end
 
   def new
-    @album = Album.find(params[:album_id])
-    @song = @album.songs.new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
     render :new
   end
 
-  # def create
-  #   @product = Product.new(product_params)
-  #   if @product.save
-  #     redirect_to products_path
-  #   else
-  #     render :new
-  #   end
-  # end
-  #
-  # def edit
-  #   @product = Product.find(params[:id])
-  #   render :edit
-  # end
-  #
-  # def show
-  #   @product = Product.find(params[:id])
-  #   render :show
-  # end
-  #
-  # def update
-  #   @product= Product.find(params[:id])
-  #   if @product.update(product_params)
-  #     redirect_to products_path
-  #   else
-  #     render :edit
-  #   end
-  # end
-  #
-  # def destroy
-  #   @product = Product.find(params[:id])
-  #   @product.destroy
-  #   redirect_to products_path
-  # end
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :show
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+  end
 
   private
     def review_params
