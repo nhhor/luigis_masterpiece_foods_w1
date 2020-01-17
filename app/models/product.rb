f216aff9c251f7d3da036d3106f3f0f33367a69b
nhhor <nhhor@post.com>
2020-01-17 11:52:09 -0800

    add cost and country validations in app-models-product.rb . 5 of 5 specs passing.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,7 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
+  validates :cost, presence: true
+  validates :country_of_origin, presence: true
 
 end
