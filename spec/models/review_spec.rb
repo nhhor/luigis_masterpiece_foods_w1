67057243978231c8cd259a11cf3ae7acf321b919
nhhor <nhhor@post.com>
2020-01-17 11:55:18 -0800

    add 3 validation tests for review_spec.rb . 3 of 8 tests failing.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,6 +4,12 @@ describe Review do
   it { should belong_to(:product) }
 end
 
+describe Review do
+  it { should validate_presence_of :author }
+  it { should validate_presence_of :content_body }
+  it { should validate_presence_of :rating }
+end
+
 # describe Division do
 #   it("titleizes the name of an employee") do
 #     division = Division.create({division_name: "giant steps"})
